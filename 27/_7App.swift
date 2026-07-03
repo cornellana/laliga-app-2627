aeffff5bdabb9de64478b29e77e851bfaafcbfdb
cornellana <cornellana@mac.com>
2026-07-03 09:15:11 +0200

    Initial Commit

---
+++ 27/_7App.swift
@@ -0,0 +1,17 @@
+//
+//  _7App.swift
+//  27
+//
+//  Created by Francisco on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct _7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 27Tests/_7Tests.swift
@@ -0,0 +1,19 @@
+//
+//  _7Tests.swift
+//  27Tests
+//
+//  Created by Francisco on 7/3/26.
+//
+
+import Testing
+@testable import Liga_26_27
+
+struct _7Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
